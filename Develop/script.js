70492d68799902a91ba61a865e1f79b63aec014a
Alex-C <alexcook1613@gmail.com>
2022-10-25 22:21:41 -0400

    finalized website styling

--- Develop/script.js
+++ Develop/script.js
@@ -15,27 +15,50 @@ var colorArr = [];
 var hoursArrMilitary;
 var currentHourMilitary;
 var militaryArr = [];
+var dynamicBackgroundClassArr = [];
+var backgroundColorClassArr = [];
 
-// saves input from user to local storage
-function saveInput(e) {
-    activityArr[e] = document.getElementById(e).value;
-    localStorage.setItem(e, activityArr[e]);
-    location.reload();
-}
 
 // Time Functions:
 
 function trackCurrentHour() {
     setInterval(function updateCurrentHour() {
-        let colorDisplay = determineColor();
+        //let colorDisplay = determineColor();
+        let backgroundClass = checkIfPastPresentOrFuture();
         currentHour = moment().format('h a');
         currentHour = currentHour.toUpperCase();
 
         for (let i = 0; i < hoursArr.length; i++) {
-            hoursMilitaryArr[i] = convertToMilitaryTime(currentHour, hoursArr[i]);
-            updateBackgroundColor('uniqueID'+i, colorDisplay[i]);
+            //hoursMilitaryArr[i] = convertToMilitaryTime(currentHour, hoursArr[i]);
+            //updateBackgroundColor('uniqueID'+i, colorDisplay[i]);
+            //console.log(backgroundClass);
+            updateBackgroundClass('middleID'+i, backgroundClass[i], 'middle');
+            //updateBackgroundClass('rightID'+i, backgroundClass[i], 'right');
         }
     }), 1000;
+}
+
+// returns dynamicBackgroundClassArr
+function checkIfPastPresentOrFuture() {
+    dynamicBackgroundClassArr = [];
+    for (let i = 0; i < hoursArr.length; i++) {
+        hoursArrMilitary = convertToMilitaryTime(hoursArr[i]);
+        militaryArr.push(hoursArrMilitary);
+        currentHourMilitary = convertToMilitaryTime(currentHour);
+
+        if (hoursArrMilitary > currentHourMilitary) {
+            dynamicBackgroundClassArr.push('future');
+        } else if (hoursArrMilitary < currentHourMilitary) {
+            dynamicBackgroundClassArr.push('past');
+        } else {
+            dynamicBackgroundClassArr.push('present');
+        }
+    }
+    return dynamicBackgroundClassArr;
+}
+
+function updateBackgroundClass(elementID, elementClass, columnPlacement) {
+    document.getElementById(elementID).className = elementClass + ' column ' + columnPlacement;
 }
 
 function convertToMilitaryTime(hour) {
@@ -63,6 +86,14 @@ function compareStrings(s1, s2) {
 
 // Local Storage Functions
 
+
+// saves input from user to local storage
+function saveInput(e) {
+    activityArr[e] = document.getElementById(e).value;
+    localStorage.setItem(e, activityArr[e]);
+    location.reload();
+}
+
 function clearLocalStorage() {
     localStorage.clear();
     location.reload();
@@ -75,8 +106,6 @@ function checkIfLocalIsNull(e) {
 }
 
 // Display/Graphic Functions
-var dynamicColor = '';
-var timeEl = document.getElementById(dynamicColor);
 
 // determines the proper color
 function determineColor() {
@@ -88,7 +117,6 @@ function determineColor() {
     
         if (hoursArrMilitary > currentHourMilitary) {
             colorArr.push('green');
-
         } else if (hoursArrMilitary < currentHourMilitary) {
             colorArr.push('grey');
         } else {
@@ -119,15 +147,25 @@ function createWorkDayScheduler() {
         activityArr.push(' ');
 
         display.push(
-            //creates hours column
-            '<div class="hour">' + hoursArr[i] + '</div>'
-            //creates description column
-            + '<div id="uniqueID'+i+'">' + '<input class="description" type="text" id="'+i+'" placeholder="'+localStorage.getItem(i)+'">'
-            //creates save button column
-            + '<input type="submit" onclick="saveInput('+i+');"></div><br>'
+            '<div class="row">'
+                // creates hours column
+                + '<div class="column left" style="color:black;">'
+                    + hoursArr[i]
+                + '</div>' 
+                //creates description column
+                + '<div class="column middle" id="middleID'+i+'">'
+                    + '<input class="description" type="text" id="'+i+'" placeholder="'+localStorage.getItem(i)+'">'
+                + '</div>'
+                // creates save button column
+                + '<div class="column right" id="rightID'+i+'">' 
+                    + '<input class="saveBtn" type="submit" onclick="saveInput('+i+');">'
+                + '</div>'
+            + '</div><br>'
         );
+
+
     };
-    containerEl.innerHTML = display.join(' ') + '<br><input type="button" value="clear" id="buttonClear" onclick="clearLocalStorage()"><br><br>';
+    containerEl.innerHTML = display.join(' ') + '<br><div style="padding-top:5%;"><input type="button" value="clear calendar" id="buttonClear" onclick="clearLocalStorage()"></div><br><br>';
 
     trackCurrentHour();
 }
